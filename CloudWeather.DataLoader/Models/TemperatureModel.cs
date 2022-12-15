4f0cdba8840da0bf50627949a66c3e4641d43961
rishavraj <fullstackdev.rishav@gmail.com>
2022-12-16 00:37:16 +0530

    Build initial data loade

---
+++ CloudWeather.DataLoader/Models/TemperatureModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CloudWeather.DataLoader.Models
+{
+    internal class TemperatureModel
+    {
+        public DateTime CreatedOn { get; set; }
+        public decimal TempHighF { get; set; }
+        public decimal TempLowF { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
